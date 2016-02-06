9545779c9195fe1f4ab145942d659212c2857949
Charles Fahselt <Charles.Fahselt@gmail.com>
2016-02-06 17:35:11 -0600

    Added basic map with walls, static player

---
+++ Super-RL-Dungeon/GameMap.cpp
@@ -0,0 +1,45 @@
+#include "libtcod.hpp"
+#include "GameMap.h"
+#include <vector>
+
+GameMap::GameMap(int w, int h) : 
+    width(w), height(h)
+{
+    // make map, single array
+    tiles = new GameTile[width*height];
+
+    for (int i = 0; i < height; i++)
+    {
+        // left wall
+        tiles[i * width].image = '#';
+
+        // right wall
+        if (i != 0) 
+            tiles[(i * width) - 1].image = '#';       
+    }
+
+    for (int i = 0; i < width; i++)
+    {
+        // top wall
+        tiles[i].image = '#';
+        // bottom wall
+        tiles[i + (width *(height-1))].image = '#';
+    }
+   
+}
+
+GameMap::~GameMap()
+{
+    delete tiles;
+}
+
+void GameMap::render()
+{
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            TCODConsole::root->setChar(x, y, tiles[x + y * width].image);
+        }
+    }
+}
--- Super-RL-Dungeon/Super-RL-Dungeon.cpp
+++ Super-RL-Dungeon/Super-RL-Dungeon.cpp
@@ -1,14 +1,20 @@
 #include "stdafx.h"
+#include "GameMap.h"
 
 int main()
 {
 	TCODConsole::initRoot(80, 50, "SUPER-RL-DUNGEON", false);
 
+	GameMap map = GameMap(80, 50);
+
 	while (!TCODConsole::isWindowClosed()) 
-	{
+	{		
 		TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, NULL, NULL);
 		TCODConsole::root->clear();
-		TCODConsole::root->putChar(40, 25, '@');
+
+		map.render();
+		TCODConsole::root->putChar(40, 45, '@');
+
 		TCODConsole::flush();
 	}
 }
